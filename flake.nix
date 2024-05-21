0eef8f9f0dd74ee4a17ffd3f54f99e9ddadb8e8e
emilpriver <emil@priver.dev>
2024-05-22 00:05:05 +0200

    Step 2, add nix testing

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    riot = {
+      url = "github:emilpriver/riot";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ flake-parts, ... }:
@@ -10,11 +14,64 @@
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
         let
-          version = "0.0.1";
+          inherit (pkgs) ocamlPackages mkShell;
+          inherit (ocamlPackages) buildDunePackage;
+          version = "0.0.1+dev";
         in
-        { 
-          devShells = {};
-          packages = {};
+        {
+          devShells = {
+            default = mkShell {
+              buildInputs = [
+                ocamlPackages.dune_3
+                ocamlPackages.ocaml
+                ocamlPackages.utop
+                ocamlPackages.ocamlformat
+                ocamlPackages.ounit2
+              ];
+              inputsFrom = [
+                self'.packages.default
+              ];
+              packages = builtins.attrValues {
+                inherit (pkgs) clang_17 clang-tools_17 pkg-config;
+                inherit (ocamlPackages) ocaml-lsp ocamlformat-rpc-lib;
+              };
+              dontDetectOcamlConflicts = true;
+            };
+          };
+          packages = {
+            default = buildDunePackage {
+              inherit version;
+              pname = "nix_template";
+              propagatedBuildInputs = [
+                inputs'.riot.packages.default
+              ];
+              src = ./.;
+            };
+            test = pkgs.stdenv.mkDerivation {
+              name = "ocaml-test";
+              buildInputs = [
+                ocamlPackages.dune_3
+                ocamlPackages.ocaml
+                ocamlPackages.utop
+                ocamlPackages.ocamlformat
+                ocamlPackages.ounit2
+              ];
+              inputsFrom = [
+                self'.packages.default
+              ];
+              src = ./.;
+              buildPhase = ''
+                dune runtest
+              '';
+              doCheck = true;
+              ## Create and output the result. for instance the coverage.txt
+              installPhase = ''
+                mkdir -p $out
+                touch $out/coverage.txt
+                echo "I am the coverage of the test" > $out/coverage.txt 
+              '';
+            };
+          };
           formatter = pkgs.nixpkgs-fmt;
         };
     };
--- test/test_nix_template.ml
+++ test/test_nix_template.ml
@@ -8,10 +8,15 @@ let say_hello () = "Hello from OCaml"
 let test_say_hello _ =
   assert_equal "Hello from OCaml" (say_hello ())
 
+(* Test case for say_hello function *)
+let test_say_hello_failure _ =
+  assert_equal "Hello from Haskell :O" (say_hello ())
+
 (* Test suite *)
 let suite =
   "Hello Tests" >::: [
-    "test_say_hello" >:: test_say_hello
+    "test_say_hello" >:: test_say_hello;
+    "test_say_hello_failure" >:: test_say_hello_failure
   ]
 
 (* Run the tests *)
